52d8f8f1620ecec8a477f46a71f7832c161f11f0
Itay Sarfaty <itaysarfaty@gmail.com>
2024-09-08 19:54:58 -0400

    chore: clean up docker and yarn files

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  images: {
-    domains: ['via.placeholder.com'],
-  },
 }
 
 export default withPayload(nextConfig)
