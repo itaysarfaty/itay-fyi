26b912d7058263fca3f34f94cd64e22a73c0bcbf
Itay Sarfaty <itaysarfaty@gmail.com>
2024-09-13 00:14:10 -0400

    fix: reduce main gap

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -6,12 +6,12 @@ import { DELAYS } from '@/lib/constants'
 // Generate an array of 10 react article titles
 export default async function Page({ searchParams }: { searchParams: { page?: string } }) {
   return (
-    <div className="p-8 flex flex-col gap-36 text-xl @lg:text-2xl @lg:gap-48 w-full container mx-auto test-red">
+    <div className="p-8 flex flex-col gap-28 text-xl @lg:text-2xl @lg:gap-[9rem] w-full container mx-auto test-red">
       <Navbar />
       <section>
-        <h2 className="leading-9">
-          i&apos;m a <Highlight delay={DELAYS[0]}>software engineer</Highlight> with 2 years of
-          experience in the logistics industry
+        <h2 className="leading-9 text-pretty">
+          i&apos;m a <Highlight delay={DELAYS[0]}>software engineer</Highlight> with 2 years of
+          experience in the logistics industry
         </h2>
       </section>
       <ArticleList page={searchParams.page ?? '1'} />
