45ecc53bdb77e23d6e69c51d38aba22e4571fe0f
Itay Sarfaty <itaysarfaty@gmail.com>
2024-09-23 21:17:27 -0400

    fix: settings placement

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,30 +11,32 @@ import { TextScaffold } from './text-scaffold'
 
 export const Footer = () => {
     return (
-        <nav className="relative pb-[40px]">
-            <TextScaffold className="justify-between">
-                <ul className="flex flex-col flex-wrap gap-x-6 gap-y-3">
-                    <Button label="home" href="/" />
-                    <Button label="about" href="/about" />
-                </ul>
-                <ul className="flex h-full flex-col items-end justify-between gap-4">
-                    <div className="flex flex-row items-center gap-4">
-                        <li>
-                            <Link href="https://www.linkedin.com/in/itaysarfaty/">
-                                <LinkedinIcon className="stroke-[0.7] hover:stroke-1" />
-                            </Link>
+        <nav className="pb-[40px]">
+            <div className="relative">
+                <TextScaffold className="justify-between">
+                    <ul className="flex flex-col flex-wrap gap-x-6 gap-y-3">
+                        <Button label="home" href="/" />
+                        <Button label="about" href="/about" />
+                    </ul>
+                    <ul className="flex h-full flex-col items-end justify-between gap-4">
+                        <div className="flex flex-row items-center gap-4">
+                            <li>
+                                <Link href="https://www.linkedin.com/in/itaysarfaty/">
+                                    <LinkedinIcon className="stroke-[0.7] hover:stroke-1" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="https://github.com/itaysarfaty">
+                                    <GithubIcon className="stroke-[0.7px] hover:stroke-1" />
+                                </Link>
+                            </li>
+                        </div>
+                        <li className="absolute bottom-0 right-0 grid place-items-center">
+                            <SettingsMenu />
                         </li>
-                        <li>
-                            <Link href="https://github.com/itaysarfaty">
-                                <GithubIcon className="stroke-[0.7px] hover:stroke-1" />
-                            </Link>
-                        </li>
-                    </div>
-                    <li className="absolute bottom-[40px] right-0 grid place-items-center">
-                        <SettingsMenu />
-                    </li>
-                </ul>
-            </TextScaffold>
+                    </ul>
+                </TextScaffold>
+            </div>
         </nav>
     )
 }
