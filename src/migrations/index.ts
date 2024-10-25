cf10d115e709a1fd4b9d6cd61505c9c7027d776b
Itay Sarfaty <itaysarfaty@gmail.com>
2024-10-24 21:20:08 -0400

    feat: add tools migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20240908_231426_init from './20240908_231426_init';
 import * as migration_20240925_040449_remove_articles from './20240925_040449_remove_articles';
 import * as migration_20241006_204725_add_projects from './20241006_204725_add_projects';
+import * as migration_20241025_011921 from './20241025_011921';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20241006_204725_add_projects.up,
     down: migration_20241006_204725_add_projects.down,
-    name: '20241006_204725_add_projects'
+    name: '20241006_204725_add_projects',
+  },
+  {
+    up: migration_20241025_011921.up,
+    down: migration_20241025_011921.down,
+    name: '20241025_011921'
   },
 ];
