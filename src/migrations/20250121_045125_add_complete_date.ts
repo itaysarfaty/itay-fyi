551483d330fdd2088ab231837692598adae5e7e7
Itay Sarfaty <itaysarfaty@gmail.com>
2025-01-20 23:52:11 -0500

    fix: new migration for completed by

--- src/migrations/20250121_045125_add_complete_date.ts
+++ src/migrations/20250121_045125_add_complete_date.ts
@@ -1,7 +1,7 @@
 import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-sqlite'
 
 export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
-  await payload.db.drizzle.run(sql`ALTER TABLE \`projects\` ADD \`completed_at\` text NOT NULL;`)
+  await payload.db.drizzle.run(sql`ALTER TABLE \`projects\` ADD \`completed_at\` text DEFAULT '2025-01-21T04:51:25.229Z' NOT NULL;`)
 }
 
 export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,5 @@
 import * as migration_20241025_015648_init from './20241025_015648_init';
-import * as migration_20250121_043815_add_complete_date from './20250121_043815_add_complete_date';
-import * as migration_20250121_044419_add_complete_date_2 from './20250121_044419_add_complete_date_2';
+import * as migration_20250121_045125_add_complete_date from './20250121_045125_add_complete_date';
 
 export const migrations = [
   {
@@ -9,13 +8,8 @@ export const migrations = [
     name: '20241025_015648_init',
   },
   {
-    up: migration_20250121_043815_add_complete_date.up,
-    down: migration_20250121_043815_add_complete_date.down,
-    name: '20250121_043815_add_complete_date',
-  },
-  {
-    up: migration_20250121_044419_add_complete_date_2.up,
-    down: migration_20250121_044419_add_complete_date_2.down,
-    name: '20250121_044419_add_complete_date_2'
+    up: migration_20250121_045125_add_complete_date.up,
+    down: migration_20250121_045125_add_complete_date.down,
+    name: '20250121_045125_add_complete_date'
   },
 ];
