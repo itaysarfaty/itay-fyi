afd48d57d0c125edfecd8b07ba78ee5d444a5aa5
Itay Sarfaty <itaysarfaty@gmail.com>
2025-08-02 20:01:11 -0400

    fix: lexical styling and user sessions

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20241025_015648_init from './20241025_015648_init';
 import * as migration_20250121_045125_add_complete_date from './20250121_045125_add_complete_date';
 import * as migration_20250405_230919_version_updates from './20250405_230919_version_updates';
+import * as migration_20250803_000039_users_sessions from './20250803_000039_users_sessions';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20250405_230919_version_updates.up,
     down: migration_20250405_230919_version_updates.down,
-    name: '20250405_230919_version_updates'
+    name: '20250405_230919_version_updates',
+  },
+  {
+    up: migration_20250803_000039_users_sessions.up,
+    down: migration_20250803_000039_users_sessions.down,
+    name: '20250803_000039_users_sessions'
   },
 ];
